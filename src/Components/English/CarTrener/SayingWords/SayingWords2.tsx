b4fe463a76cbfaca5d7212b1405419ac15723191
zharikovartem <42375825+zharikovartem@users.noreply.github.com>
2021-04-15 17:27:24 +0300

    1

--- src/Components/English/CarTrener/SayingWords/SayingWords2.tsx
+++ src/Components/English/CarTrener/SayingWords/SayingWords2.tsx
@@ -16,69 +16,175 @@ var synth = window.speechSynthesis;
 
 const SayingWords: React.FC<LerningWordsPropsType> = (props) => {
     const [record, setRecord] = useState<string>()
-    const [status, setStatus] = useState(false)  
+    const [status, setStatus] = useState(false)  // запущен ли recognition
     const [recognition, setRecognition] = useState(new SpeechRecognition())
+
     const [tergetName, setTergetName] = useState<string>(props.rand ? props.target.name : props.target.relations[0].name)
+    const [targetAnswer, setTargetAnswer] = useState<string>(props.rand ? props.target.relations[0].name : props.target.name)
+    const [targetLang, setTargetLang] = useState<"ru-RU"|"en-US">()
+    const [ansswerLang, setAnswerLang] = useState<"ru-RU"|"en-US">()
+    
     const [voices] = useState( window.speechSynthesis.getVoices() )
     const [ok, setOk] = useState(false)
 
+    const [recognizing, setRecognizing] = useState(false)
+    // let recognizing = false;
+
+    const [speachResults, setSpeachResults] = useState<Array<string>>([])
+
     useEffect(()=> {
+        console.log(recognizing)
+        if (!recognizing) {
+            recognition.start()
+            // console.log('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!started')
+            setRecognizing(true)
+        }
+    },[recognizing])
+
+    useEffect(()=> {
+        console.log('!!!!!!!!!!!!!!!!!!!props.target changed')
         setTergetName(props.rand ? props.target.name : props.target.relations[0].name)
+        setTargetAnswer(props.rand ? props.target.relations[0].name : props.target.name)
+        setTargetLang(!props.rand ? "ru-RU":"en-US")
+        setAnswerLang(props.rand ? "ru-RU":"en-US")
+        // setStatus(true)
+
+        // if (props.isShowAudio) {
+        //     speak(utterThis)
+        // }
+
+        const newSpechInstanse = newSpech(props.rand ? props.target.name : props.target.relations[0].name)
+        const lang = !props.rand ? "ru-RU":"en-US"
+        newSpechInstanse.voice = voices.filter(item => item.lang === lang)[0]
+        console.log('??????????????????????????????', newSpechInstanse)
+        speak(newSpechInstanse, true)
+
+        recognition.lang = props.rand ? "ru-RU":"en-US"
+
     },[props.target])
 
-        recognition.lang = props.rand ? 'ru-RU' : 'en-US'
+        recognition.interimResults = true
+        // recognition.lang = props.rand ? 'ru-RU' : 'en-US'
 
         recognition.onaudiostart = function () {
             setStatus(true)
+            // setRecognizing(true);
+            // console.log('setRecognizing(true)')
             // console.log('onaudiostart')
         }
 
-        recognition.onaudioend = ()=> {
-            // console.log('onaudioend')
-            // recognition.start()
-        }
+        recognition.onaudioend = ()=> {}
 
         recognition.onresult = function(event) {
-            // console.log('onresult: ')
+            console.log('onresult: ')
             for(let i=0; i<event.results.length; i++) {
-                setStatus(false)
-                // setOk(false)
-                console.log('!!!!!: ',event.results[i][0].transcript)
-                // setRecord(event.results[i][0].transcript)
-                
-                // if (record !== undefined) {
-                
-                
-                const newSpechInstanse = newSpech(event.results[i][0].transcript)
-                const lang = !props.rand ? "en-US" : "ru-RU"
-                console.log(lang)
-                newSpechInstanse.voice = voices.filter(item => item.lang === lang)[0]
-                console.log(newSpechInstanse)
-                
+                // setStatus(false)
+                // recognition.abort()
+                // console.log('stop()')
 
-                recognition.abort()
-                console.log('stop()')
-
-                speak(newSpechInstanse, true)
-                console.log('speak')
+                // console.log('--------------------: ',event.results[i][0].transcript)
+                // console.log(i,')',event.results[i][0].transcript,': ',{...event.results[i]})
+                let speachResultsCopy = [...speachResults]
+                speachResultsCopy.push(event.results[i][0].transcript)
+                setSpeachResults(speachResultsCopy)
                 
+                // const newSpechInstanse = newSpech(event.results[i][0].transcript)
+                // const lang = !props.rand ? "en-US" : "ru-RU"
+                // newSpechInstanse.voice = voices.filter(item => item.lang === lang)[0]
+                // speak(newSpechInstanse, true)
             }
+            // console.log(event.results)
         }
 
         recognition.onend = ()=> {
-            console.log('onend')
+            console.log('onend', recognizing)
+            // console.log('onend statusToSpeach: ', status)
+            // console.log(speachResults)
             if (status) {
-                setStatus(false)
+                console.log('начинаем сравнивать')
+                // checking targetAnswer vs speachResults
+                let success = false;
+                speachResults.forEach(result => {
+                    // console.log(result)
+                    if (result.toLocaleLowerCase().includes(targetAnswer.toLocaleLowerCase()) ) {
+                        // console.log('!!!!!!!!!!',result)
+                        success = true
+                    }
+                })
+
+                const lastResult = speachResults[speachResults.length-1]
+                console.log('speachResults: ', [...speachResults])
+                // console.log('lastResult: ', lastResult)
+                setSpeachResults([])
+
+                if (success) {
+                    // recognition.abort()
+                    // setRecognizing(false)
+                    console.log('success = true')
+                    const correctResult = !props.rand ? '. Correct result!':'. Правельный результат!'
+                    const newSpechInstanse = newSpech(lastResult + correctResult)
+                    const lang = !props.rand ? "en-US" : "ru-RU"
+                    newSpechInstanse.voice = voices.filter(item => item.lang === lang)[0]
+                    speak(newSpechInstanse, true)
+
+                    props.checkTestResult({
+                        result: 'success',
+                        checkMethod: !props.rand ? 'en_ru_c' : 'ru_en_c'
+                    }, props.target.id)
+                    console.log('props.next(1)')
+                    props.next(1)
+                    // setStatus(false)
+                } else {
+                    console.log('success = false')
+                    const isComand = lastResult !== undefined ? checkComands(lastResult) : ''
+
+                    if (isComand === '') {
+                        if (lastResult!== undefined) {
+                            console.log(lastResult.toLowerCase())
+                            // recognition.abort()
+                            // setRecognizing(false)
+                            // console.log('!!!setRecognizing(false)', recognizing)
+                            const newSpechInstanse = newSpech(!props.rand ? 'wrong!':'Не верно!')
+                            const lang = !props.rand ? "en-US" : "ru-RU"
+                            newSpechInstanse.voice = voices.filter(item => item.lang === lang)[0]
+                            console.log(newSpechInstanse)
+                            speak(newSpechInstanse, true)
+                            // recognition.start()
+                            setRecognizing(true)
+                            console.log('!!!setRecognizing(true)', recognizing)
+                        } else {
+                            recognition.stop()
+                            console.log('stop')
+                            recognition.start()
+                            console.log('start')
+                            // setRecognizing(false)
+                            // console.log('setRecognizing(true)')
+                        }
+                    } else {
+                        console.log(isComand)
+                        recognition.abort()
+                        // setRecognizing(false)
+                        console.log('setRecognizing(false)')
+                        const newSpechInstanse = newSpech(isComand)
+                        const lang = !props.rand ? "en-US" : "ru-RU"
+                        newSpechInstanse.voice = voices.filter(item => item.lang === lang)[0]
+                        speak(newSpechInstanse, true)
+                    }
+                    
+                }
+
+                // setStatus(false)
                 // console.log('!!!!!!!!', status)
-                recognition.start()
+                // recognition.start()
+            } else {
+                console.log('НАДО ЗАПУСТИТЬ')
             }
         }
 
         recognition.onerror = function(event) {
-            // console.log('Speech recognition error detected: ' + event.error);
-            // if (event.error === 'no-speech') {
-            //     recognition.start()
-            // }
+            console.log('Speech recognition error detected: ' + event.error);
+            recognition.stop()
+            setRecognizing(false)
         }
 
         recognition.onnomatch = function() {
@@ -88,7 +194,9 @@ const SayingWords: React.FC<LerningWordsPropsType> = (props) => {
             // console.log('Some sound is being received');
         }
         recognition.onsoundend = function() {
-            console.log('Sound has stopped being received');
+            console.log('Звук перестал приниматься', recognizing);
+            // recognition.start()
+            // setRecognizing(false)
         }
 
         
@@ -101,9 +209,23 @@ const SayingWords: React.FC<LerningWordsPropsType> = (props) => {
         utterThis.voice = voices.filter(item => item.lang === lang)[0]
         // 
         
-        const startLisent = () => {
-            console.log('recognition.start()')
-            recognition.start()
+        const startLisent = (check: string|undefined = undefined ) => {
+            setRecognizing(false)
+            // if (check === undefined) {
+            //     // console.log('recognition.start()', recognition)
+            //     console.log('!!!!!!!!!!!!!!!!!!!!!!!!!!recognizing', !recognizing)
+            //     // console.log('ansswerLang', ansswerLang)
+            //     if (!recognizing) {
+            //         // recognition.start()
+            //         setRecognizing(true)
+            //         console.log('setRecognizing(true)                      startLisent')
+            //     } else {
+            //         console.log('НЕ ЗАПУСТИЛИ СТАРТ', recognizing)
+            //         // recognition.start()
+            //     }
+            // } else {
+            //     console.log('Проверяем: ', check)
+            // }
         }
 
         const newSpech = (text: string): SpeechSynthesisUtterance => {
@@ -111,6 +233,7 @@ const SayingWords: React.FC<LerningWordsPropsType> = (props) => {
         }
         
         const speak = (utterThisItem: SpeechSynthesisUtterance, need?: boolean) => {
+            recognition.abort()
             if (!ok || need) {
                 window.speechSynthesis.speak(utterThisItem)
                 console.log('speechSynthesis.speak: ', utterThisItem.text)
@@ -119,16 +242,23 @@ const SayingWords: React.FC<LerningWordsPropsType> = (props) => {
             }
         }
 
-        if (props.isShowAudio) {
-            speak(utterThis)
-        }
+        // if (props.isShowAudio) {
+        //     speak(utterThis)
+        // }
+
+        // console.log('speachResults: ', speachResults)
 
     return (
         <div>
             v1.34) SayingWords<br/>
+            <h4>tergetName: {tergetName}= targetAnswer:{targetAnswer}</h4>
             <p>{record}</p>
+            <p>Язык на котором сказали текст: <b>{targetLang}</b> ({!targetLang})</p>
+            <p>Язык восприятия речи: <b>{ansswerLang}</b></p>
+            <p>Реальный Язык восприятия речи: <b>{recognition.lang}</b></p>
+            <p>Запись звука: {recognizing ? 'Да' : 'Нет'}</p>
         
-        <Button className="btntooc" type="primary" onClick={startLisent}>speak</Button>
+        <Button className="btntooc" type="primary" onClick={()=>{startLisent()}}>speak</Button>
         </div>
     )
 }
@@ -137,13 +267,24 @@ export default SayingWords
 
 const checkPendidng = (speechSynthesis: SpeechSynthesis, startLisent:()=>void) => {
     setTimeout(() => {
-        // console.log(window.speechSynthesis)
-        if (speechSynthesis.pending) {
+        if (speechSynthesis.speaking || speechSynthesis.pending) {
             checkPendidng(speechSynthesis, startLisent)
-            // console.log(window.speechSynthesis)
+            console.log('WAIT')
         } 
         else {
+            // console.log('startLisent from checkPendidng')
             startLisent()
         }
     }, 100);
+}
+
+const checkComands = (lastResult:string): string => {
+    if (lastResult.toLocaleLowerCase().includes('next') ) {
+        return 'next'
+    }
+    if (lastResult.toLocaleLowerCase().includes('следующий') ) {
+        return 'следующий'
+    }
+
+    return ''
 }
